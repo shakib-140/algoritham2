5b6c3a7bc1cf13d52bd68205679a02c7a8c9316b
Shakirrojjaman <95244399+shakib-140@users.noreply.github.com>
2023-01-31 23:51:36 +0600

    Add files via upload

---
+++ Dijkstra_code.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N=1e5+5;
+const int INF=1e9;
+vector<pair<int,int>>adj_list[N];
+int visited[N];
+int nodes,edges;
+int d[N];
+void dijkstra(int src);
+void dijkstra(int src)
+{
+    for(int i=1; i<=nodes; i++)
+    {
+        d[i]=INF;
+    }
+    d[src]=0;
+    for(int i=0; i<nodes; i++)
+    {
+        int selected_node=-1;
+        for(int j=1; j<=nodes; j++)
+        {
+            if(visited[selected_node]==1)continue;
+            if(selected_node == -1 || d[selected_node] > d[j])
+            {
+                selected_node=j;
+            }
+        }
+        visited[selected_node]=1;
+        for(auto adj_entry:adj_list[selected_node])
+        {
+            int adj_node=adj_entry.first;
+            int adj_cost=adj_entry.second;
+            if(d[selected_node]+adj_cost<d[adj_node])
+            {
+                d[adj_node]=d[selected_node]+adj_cost;
+            }
+        }
+    }
+
+}
+int main()
+{
+    cin>>nodes>>edges;
+    for(int i=0; i<edges; i++)
+    {
+        int u,v,w;
+        cin>>u>>v>>w;
+        adj_list[u].push_back({v,w});
+        adj_list[v].push_back({u,w});
+    }
+    int src=1;
+    dijkstra(src);
+    for(int i=1;i<nodes;i++)
+    {
+        cout<<d[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
